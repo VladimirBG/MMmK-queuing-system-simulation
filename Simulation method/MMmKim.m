0bdf2dccac5abad644b99d84abbc9eedee9fdf5d
VladimirBG <bukharkinv@gmail.com>
2017-01-03 06:13:21 +0300

    Modify algorithm

--- Simulation method/MMmKim.m
+++ Simulation method/MMmKim.m
@@ -2,7 +2,7 @@ function MMmKim
 clc, close, clear
 
 %% Задание значений начальных параметров СМО
-DialogM = inputdlg({'Средняя интенсивность потока поступления заявок (Lambda)' 'Средняя интенсивность потока обслуживания заявок (Mu)' 'Максимальная длина очереди (m)' 'Допустимое число требований в системе (K)' 'Время моделирования СМО' 'Число повторов имитации'}, 'Введите начальные параметры системы', [1; 1; 1; 1; 1; 1], {'1.254' '0.756' '3' '5' '60' '50'});
+DialogM = inputdlg({'Средняя интенсивность потока поступления заявок (Lambda)' 'Средняя интенсивность потока обслуживания заявок (Mu)' 'Максимальная длина очереди (m)' 'Допустимое число требований в системе (K)' 'Время моделирования СМО' 'Число повторов имитации'}, 'Введите начальные параметры системы', [1; 1; 1; 1; 1; 1], {'1.254' '0.756' '3' '5' '20' '50'});
 DialogT = inputdlg({'От T1' 'До T2'}, 'Интервал времени обслуживания приборов [T1 - T2]', [1 85; 1 85], {'5', '10'});
 
 L = str2num(DialogM{1}); % Интенсивность поступления заявок
@@ -41,61 +41,70 @@ stairs(treq);
 
 %% Цикл для каждого момента времени treq
 for i = 1 : length(treq)
-    tcur = treq(i);
-    for ds = 1 : length(Dstatus) % Если заявка обработалась, то статус прибора = 0
-        if not(Dstatus(ds) > tcur)
-            if not(Dstatus(ds) == 0)
+    
+    tcur = 0;
+    while tcur < treq(i)
+        
+        tcurCorrected = Dstatus(1);  % Ближайшее время
+        for ds = 1 : length(Dstatus)
+             if not(Dstatus(ds) == 0)
+                if Dstatus(ds) < tcurCorrected
+                    tcurCorrected = Dstatus(ds);
+                end
+             end
+        end 
+
+        if tcurCorrected < treq(i) && tcurCorrected > 0
+                tcur = tcurCorrected;
+        else
+            tcur = treq(i);
+        end
+        
+        for ds = 1 : length(Dstatus) % Если заявка обработалась, то статус прибора = 0
+            if Dstatus(ds) <= tcur && Dstatus(ds) > 0
                 Cserv = Cserv + 1;
-            end
-            Dstatus(ds) = 0;
-        end
-    end
-    
-    % Проверка наличия свободных приборов
-    busyFlag = true;
-    rng('shuffle');
-    tserv = randi([T1,T2]);
-    for ds = 1 : length(Dstatus)
-        if Dstatus(ds) == 0
-            Dstatus(ds) = tcur + tserv;
-            busyFlag = false; 
-            break;
-        end
-    end
-    
-    if busyFlag  % Если все прибоы заняты, то инкрементируется счетчик очереди
-        Queues = Queues + 1;
-    end
-    
-    if busyFlag && Queues > m %Если превысило очередь
-        Crejected = Crejected + 1;
-        Queues = Queues - 1;
-    else
-        if Queues > 0  % если есть очередь
-            for qu = 1 : Queues
-                rng('shuffle');
-                tserv = randi([T1,T2]);
-                for ds = 1 : length(Dstatus)
-                    if Dstatus(ds) == 0
-                        Dstatus(ds) = tcur + tserv;
-                        break;
-                    end
-                end 
+                Dstatus(ds) = 0;
             end
         end
-    end
-    
-    tcurCorrected = Dstatus(1);  % Ближайшее время
-    for ds = 1 : length(Dstatus)
-         if not(Dstatus(ds) == 0)
-            if Dstatus(ds) < tcurCorrected
-                tcurCorrected = Dstatus(ds);
+
+        % Проверка наличия свободных приборов
+        busyFlag = true;
+        rng('shuffle');
+        tserv = 1/M*log(randi([T1,T2]));
+        for ds = 1 : length(Dstatus)
+            if Dstatus(ds) == 0
+                Dstatus(ds) = tcur + tserv;
+                busyFlag = false; 
+                break;
             end
-         end
-     end 
-    
-    
-    
-    
-    display(Dstatus);
+        end
+
+        if busyFlag  % Если все прибоы заняты, то инкрементируется счетчик очереди
+            Queues = Queues + 1;
+        end
+
+        if busyFlag && Queues > m %Если превысило очередь
+            Crejected = Crejected + 1;
+            Queues = Queues - 1;
+        else
+            if Queues > 0 && not(busyFlag) % если есть очередь
+                Qvalue = Queues;
+                for qu = 1 : Queues 
+                    rng('shuffle');
+                    tserv = 1/M*log(randi([T1,T2]));
+                    for ds = 1 : length(Dstatus)
+                        if Dstatus(ds) == 0
+                            Dstatus(ds) = tcur + tserv;
+                            Queues = Queues - 1;
+                            break;
+                        end
+                    end 
+                end
+            end
+        end 
+    end 
 end
+
+display(Cserv);
+display(Crejected);
+display(Creq);
