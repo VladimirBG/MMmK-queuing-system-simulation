7be6c9c0b5996f167274efbcc0a8f1aa23d6a603
VladimirBG <bukharkinv@gmail.com>
2017-01-03 04:54:56 +0300

    Adds analytical method of MMmK simulation

---
+++ Analytical method/mmmkFinal.m
@@ -0,0 +1,95 @@
+function mmmk
+clc,close 
+% Начальные параметры системы
+DialogM = inputdlg({'Средняя интенсивность потока поступления заявок (Lambda)' 'Средняя интенсивность потока обслуживания заявок (Mu)' 'Максимальная длина очереди (m)' 'Допустимое число требований в системе (K)' 'Время работы системы'}, 'Введите начальные параметры системы');
+
+L = str2num(DialogM{1});
+M = str2num(DialogM{2});
+m = str2num(DialogM{3});
+K = str2num(DialogM{4}); 
+%%%%%%%%%%%%%%%%%%%%%%%%
+global Pij
+Pij = zeros(K+1,K+1)';
+Pij(1, 1:2) = [-L, M]; 
+I = 1;
+while I < K+1
+    I = I+1;
+    if I == K+1
+         Pij(I, I-1) = L;
+         Pij(I, I) = -M*(K-m+1);
+    elseif I <= K-m+1
+        Pij(I, I-1) = L;
+        Pij(I, I) = -(L+M*(I-1));
+        Pij(I, I+1) = M*I;
+        else
+            Pij(I, I-1) = L;
+            Pij(I, I) = -(L+M*(K-m+1));
+            Pij(I, I+1) = M*(K-m+1);
+    end   
+end
+
+%% Численное интегрирование дифф. уравнений
+%% P0 - Начальные условия
+P0 = [1;zeros(length(Pij)-1,1)];
+T = [0,str2num(DialogM{5})];
+[t,P] = ode23(@cmo, T, P0);
+ 
+%% Построение диаграммы вероятностей состояний
+%% plot %% с различными цветами
+plot(t,P, 'LineWidth', 2);
+
+grid on
+N = length(Pij)-1;
+arr = [0:N]';
+str = num2str(arr);
+legend(strcat('\bf\itP\rm\bf_', str, '(\itt\rm\bf)'));
+title(sprintf('%s Вероятности состояний системы M/M/%d/%d', '\bf\fontsize{12}',m, K));
+xlabel('\bf\it\fontsize{12}  t')
+ylabel('\bf\fontsize{12}\itP\rm\bf(\itt\rm\bf)');
+set(gca, 'fontweight','bold', 'fontsize',10)
+
+fprintf('\n Стационарные вероятности:\n');
+for J = 1 : length(Pij)
+    fprintf('\tP%d = %f\n', J-1, P(end,J));
+end
+
+%% Финальные вероятности системы
+fprintf('\n Финальные вероятности:\n');
+A = Pij;
+A(K+2,:)=ones(1,length(Pij));
+fa = zeros(1,length(Pij))';
+fa(K+2,:) = 1;
+Pf = vpa(mldivide(A, fa), 7);
+for J = 1 : length(Pf)
+    fprintf('\tPf%d = %f\n', J-1, Pf(J));
+end
+
+
+fprintf('\n\t ОПЕРАЦИОННЫЕ ХАРАКТЕРИСТИКИ:\n');
+Pnot = P(end,end);
+fprintf(' Вероятность отказа Pnot = %f\n', P(end,end));
+Q = 1 - Pnot;
+fprintf(' Относительная пропускная способность Q = %f\n', Q);
+Ab = L*Q;
+fprintf(' Абсолютная пропускная способность A = %f\n', Ab);
+Pq = sum(P(end, m+1:end));
+fprintf(' Вероятность наличия очереди Pq = %f\n', Pq);
+Ps = sum(P(end, m:end));
+fprintf(' Вероятность загрузки всех каналов обслуживания Ps = %f\n', Ps);
+Ns = [0:length(Pij)-1]*P(end,:)';
+fprintf(' Среднее количество требований в системе Ns = %f\n', Ns);
+fprintf(' Среднее время пребывания требования в системе Ts = %f\n', Ns/L);
+Nq = [0:(K-m)]*P(end,m:K)';
+fprintf(' Средняя длина очереди Nq = %f\n', Nq);
+fprintf(' Среднее время пребывания требования в очереди Tq = %f\n', Nq/L);
+fprintf('Поступило заявок: %f\n', L);
+fprintf('Среднее количество отклоненных заявок: %f\n', DialogM{5}*Pnot);
+
+
+
+
+function f = cmo(t,P);
+%% Функция описания правых частей 
+%% дифференциальных уравнений
+global Pij
+f = Pij*P;
